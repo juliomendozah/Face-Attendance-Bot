6f408adf827b8990a67be5d94ade0b15ae8b16e0
juliomendozah <julio.mendozah@pucp.edu.pe>
2021-11-04 19:42:36 -0500

    Demo

--- emotion detector_Meraki.py
+++ emotion detector_Meraki.py
@@ -1,11 +1,32 @@
 import cv2
 from deepface import DeepFace
+import requests
 
 faceCascade=cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
+def get_rtspurl(cam_serial):
+    """
+    Get RTSP URL from camera
+    """
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-Cisco-Meraki-API-Key": meraki_api_key
+    }
+
+    try:
+        r_rtspurl = requests.request('GET', f"https://api.meraki.com/api/v1/devices/{cam_serial}/camera/video/settings", headers=headers)
+        r_rtspurl_json = r_rtspurl.json()
+        return r_rtspurl_json["rtspUrl"]
+    except Exception as e:
+        return print(f"Error when getting image URL: {e}")
+
+meraki_api_key = "0d7a8b4276fb04606fe0659a37e52dbba345e805"
+cam_serial = "Q2FV-NX7G-MNB2"
+
 if __name__ == "__main__":
     # Get a reference to webcam #0 (the default one)
-    video_capture = cv2.VideoCapture(0)
+    video_capture = cv2.VideoCapture(get_rtspurl(cam_serial))
     process_this_frame = True
 
     while True:
